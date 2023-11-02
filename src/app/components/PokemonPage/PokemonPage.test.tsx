b16093103195a26b1723369a9957ee339573a169
Jack <jackw221001@Gmail.com>
2023-11-02 23:28:09 +0000

    removed unecesarry props

--- src/app/components/PokemonPage/PokemonPage.test.tsx
+++ src/app/components/PokemonPage/PokemonPage.test.tsx
@@ -4,16 +4,6 @@ import PokemonPage from "./PokemonPage";
 describe("PokemonPage", () => {
   test("Checks component renders correctly", async () => {
     const pokemonProps = {
-      previous: {
-        name: "bulbasaur",
-        url: "https://pokeapi.co/api/v2/pokemon/1/",
-        id: 1,
-      },
-      next: {
-        url: "https://pokeapi.co/api/v2/pokemon/3/",
-        name: "venusaur",
-        id: 3,
-      },
       name: "ivysaur",
     };
     const jsx = await PokemonPage(pokemonProps);
